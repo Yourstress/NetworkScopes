6352b522ffe8451678b4c68af9640d02d9794826
Sour <sour@diwaniyalabs.com>
2017-06-20 22:55:16 +0000

    Generated examples.

--- Assets/NetworkScopes/Example/Simple Lobby/IMyClientLobby.cs
+++ Assets/NetworkScopes/Example/Simple Lobby/IMyClientLobby.cs
@@ -5,4 +5,6 @@ public interface IMyClientLobby : IClientScope
 {
     // this is a Signal thst is manually called at a later time.
     void FoundMatch(LobbyMatch match);
+
+    void FoundMatches(LobbyMatch[] matches);
 }
