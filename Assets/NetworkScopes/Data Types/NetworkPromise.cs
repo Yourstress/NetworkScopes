7cdc82f5d941b7b9d4e3378afc2f6f6bdaf4dbdf
Sour <yourstress@gmail.com>
2021-05-15 04:40:40 +0300

    [NetworkPromise] Added GetAsync method.

--- Assets/NetworkScopes/Data Types/NetworkPromise.cs
+++ Assets/NetworkScopes/Data Types/NetworkPromise.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NetworkScopes
@@ -58,21 +59,43 @@ namespace NetworkScopes
     }
     public class ObjectPromise<T> : INetworkPromise<T> where T : ISerializable, new()
     {
+        private T lastValue;
         private Action<T> _onReceivePromise;
-
+        
+        private int lastValueId = 0;
         void INetworkPromise.Receive(ISignalReader reader)
         {
-            T value = reader.ReadObject<T>();
+            T value = lastValue = reader.ReadObject<T>();
+
+            lastValueId++;
 
             if (_onReceivePromise != null)
                 _onReceivePromise(value);
-            else
-                NSDebug.Log("Received unhandled promise");
+            // else
+                // NSDebug.Log("Received unhandled promise");
         }
 
         public void ContinueWith(Action<T> onReceivePromise)
         {
             _onReceivePromise += onReceivePromise;
+        }
+        
+        public async System.Threading.Tasks.Task<T> GetAsync(CancellationToken cancelToken, int timeoutInSeconds = 3)
+        {
+            DateTime cutoffTime = DateTime.Now.AddSeconds(timeoutInSeconds);
+
+            int valueId = lastValueId;
+
+            do
+            {
+                await System.Threading.Tasks.Task.Delay(1, cancelToken);
+            }
+            while (valueId == lastValueId && DateTime.Now < cutoffTime);
+            
+            if (DateTime.Now >= cutoffTime)
+                NSDebug.LogError("Time expired!");
+            
+            return lastValue;
         }
     }
 }
